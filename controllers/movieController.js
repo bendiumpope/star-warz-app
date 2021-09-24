b6b2e828e694a31ce9066f7b48d3c3641c1b3afe
Mac <mac@Macs-MacBook-Pro.local>
2021-09-24 01:06:27 +0100

    create getAll endpoint to Get movie list

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -4,8 +4,27 @@ exports.getAll = async (req, res, next) => {
     try {
       const response = await axios.get("https://swapi.dev/api/films");
 
-      const data = JSON.stringify(response.data)
-      return res.status(200).json(data);
+      const responseData = response.data.results.reduce((acc, movie) => {
+        let movieObj = {
+          episode_id: movie.episode_id,
+          name: movie.title,
+          opening_crawl: movie.opening_crawl,
+          release_date: movie.release_date,
+          characters: movie.characters,
+          comment_counts: 0,
+          comments: []
+        };
+        acc.push(movieObj)
+
+        return acc;
+      }, [])
+
+      responseData.sort((a, b) => {
+        let dateA = new Date(a.release_date), dateB = new Date(b.release_date)
+        return dateA - dateB;
+      });
+      // const data = JSON.stringify(responseData)
+      return res.status(200).json(responseData);
     } catch (error) {
       console.error(error);
     }
